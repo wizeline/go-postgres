05092f9937354ed4c65b106fbf7f08a42cf58988
luisc09 <luis.carbajal@wizeline.com>
2020-10-08 17:25:51 -0500

    feat: set same context for all endpoints

--- router/router.go
+++ router/router.go
@@ -13,9 +13,9 @@ func Router() *mux.Router {
 
 	router.HandleFunc("/api/user/{id}", middleware.GetUser).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/user", middleware.GetAllUser).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/newuser", middleware.CreateUser).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/user", middleware.CreateUser).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/user/{id}", middleware.UpdateUser).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/api/deleteuser/{id}", middleware.DeleteUser).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/user/{id}", middleware.DeleteUser).Methods("DELETE", "OPTIONS")
 
 	return router
 }
